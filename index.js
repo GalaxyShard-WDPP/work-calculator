f64a37238b742f881313f191254707498adfeeae
GalaxyShard <galaxyshard.tech@proton.me>
2023-09-29 14:59:12 -0400

    started js

--- index.js
+++ index.js
@@ -3,5 +3,17 @@
 let mathBtns = document.getElementsByClassName("math-btn");
 let numBtns = document.getElementsByClassName("num-btn");
 let equalBtn = document.getElementsByClassName("equal-btn")[0];
+let entry = document.getElementById("entry");
 
 
+function handleNumber(symbol) {
+    if (entry.textContent == "0" && symbol != ".") {
+        entry.textContent = symbol;
+        return;
+    }
+    entry.textContent += symbol;
+
+}
+for (let btn of numBtns) {
+    handleNumber(btn.dataset.symbol);
+}
