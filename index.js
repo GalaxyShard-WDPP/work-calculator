5ef100469c03304e42fc3d5b78fdfd1303d02728
GalaxyShard <galaxyshard.tech@proton.me>
2023-10-03 13:25:49 -0400

    implemented basic function

--- index.js
+++ index.js
@@ -5,15 +5,80 @@ let numBtns = document.getElementsByClassName("num-btn");
 let equalBtn = document.getElementsByClassName("equal-btn")[0];
 let entry = document.getElementById("entry");
 
-
+let mem = null;
+let savedNum = null;
+let operator = null;
+let clearOnNext = false;
 function handleNumber(symbol) {
-    if (entry.textContent == "0" && symbol != ".") {
-        entry.textContent = symbol;
+    if (clearOnNext) {
+        entry.value = "0";
+        clearOnNext = false;
+    }
+    let decimalPlaced = entry.value.indexOf(".") != -1;
+    if (decimalPlaced) {
+        if (symbol === ".") {
+            return;
+        }
+    }
+    if (entry.value === "0" && symbol !== ".") {
+        entry.value = symbol;
         return;
     }
-    entry.textContent += symbol;
+    entry.value += symbol;
 
 }
-for (let btn of numBtns) {
-    handleNumber(btn.dataset.symbol);
+function handleOperator(symbol) {
+    if (symbol === "AC") {
+        clearOnNext = false;
+        savedNum = null;
+        entry.value = "0";
+        operator = null;
+        return;
+    }
+    if (symbol === "MC") {
+        mem = entry.value;
+        return;
+    }
+    if (symbol === "MP") {
+        if (mem !== null) {
+            entry.value = mem;
+        }
+        return;
+    }
+    if (savedNum !== null) {
+        handleEvaluate();
+    }
+    savedNum = parseFloat(entry.value);
+    operator = symbol;
+    clearOnNext = true;
 }
+function handleEvaluate() {
+    if (savedNum === null) {
+        return;
+    }
+    let num = parseFloat(entry.value);
+
+    if (operator === "+") {
+        entry.value = savedNum + num;
+    } else if (operator === "-") {
+        entry.value = savedNum - num;
+    } else if (operator === "*") {
+        entry.value = savedNum * num;
+    } else if (operator === "/") {
+        entry.value = savedNum / num;
+    } else {
+        console.error(`Error: operator '${operator}' does not exist`);
+        return;
+    }
+    operator = null;
+    savedNum = null;
+}
+for (let btn of numBtns) {
+    btn.addEventListener("click", () => { handleNumber(btn.dataset.symbol); });
+
+}
+for (let btn of mathBtns) {
+    btn.addEventListener("click", () => { handleOperator(btn.dataset.symbol); });
+}
+
+equalBtn.addEventListener("click", handleEvaluate);
